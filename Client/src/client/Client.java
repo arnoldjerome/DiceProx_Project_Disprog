708590c7b5ed4a504deb3125a4bb1236bf853dc1
arnoldjerome <157452200+arnoldjerome@users.noreply.github.com>
2024-06-11 20:18:25 +0700

    Project Kosong

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -10,16 +10,13 @@ package client;
  */
 public class Client {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("KITA GANTENG YUHU");
-        System.out.println("WOI NYOBA");
-        halo bang -malv
-        hai ngab MWAHH -gio
-        halo ini rome terakhir
+        
     }
     
 }
