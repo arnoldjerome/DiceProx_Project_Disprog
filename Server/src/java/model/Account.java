087e1a08fbca7442b81e266ecb7c50a5d2b62a1a
arnoldjerome <157452200+arnoldjerome@users.noreply.github.com>
2024-06-13 22:16:27 +0700

    tambahan untuk class

---
+++ Server/src/java/model/Account.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rome
+ */
+public class Account extends MyModel{
+    
+    int UserID;
+    private String Username;
+    private String Password;
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public Account(int UserID, String Username, String Password) {
+        this.UserID = UserID;
+        this.Username = Username;
+        this.Password = Password;
+    }
+    
+    
+    
+
+   
+    
+}
---
+++ Server/src/java/model/MyModel.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rome
+ */
+public abstract class MyModel {
+    protected static Connection conn;
+    protected Statement statement;
+    protected ResultSet result;
+
+    public MyModel(Statement statement, ResultSet result) {
+        this.statement = statement;
+        this.result = result;
+    }
+    
+    public MyModel(){
+        this.conn = this._getConnection();
+        this.statement = null;
+        this.result = null;
+    }
+
+    public static Connection getConn() {
+        return conn;
+    }
+
+    public static void setConn(Connection conn) {
+        MyModel.conn = conn;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Statement statement) {
+        this.statement = statement;
+    }
+
+    public ResultSet getResult() {
+        return result;
+    }
+
+    public void setResult(ResultSet result) {
+        this.result = result;
+    }
+    
+    private Connection _getConnection()
+    {
+        if (MyModel.conn == null){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+                return DriverManager.getConnection("jdbc:mysql://localhost/diceprox_ticketing", "root", "");
+            }
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        return MyModel.conn;
+    }
+    
+    
+    
+    
+}
