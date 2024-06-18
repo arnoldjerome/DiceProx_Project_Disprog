7f5b006032d295bae5e600b426b977e96c6fea0c
fxsurya27 <fxsurya27@gmail.com>
2024-06-18 23:59:47 +0700

    FIX penamaan

--- Client/src/diceprox_main/mainForm.java
+++ Client/src/diceprox_main/mainForm.java
@@ -134,6 +134,8 @@ public class MainForm extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
